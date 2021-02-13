28b1da87718124d0e26f6d5bac497ac085598538
alex <alex@pop-os.localdomain>
2021-02-13 13:56:50 +0100

    add completed list1.py

--- session2/list1.py
+++ session2/list1.py
@@ -17,8 +17,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-    # +++your code here+++
-    return 
+    count_of_matches = 0
+    for word in words:
+         if len(word) >= 2 and word[0] == word[-1]:
+               count_of_matches += 1
+    return count_of_matches
 
 
 # B. front_x
@@ -29,8 +32,20 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    # +++your code here+++
-    return 
+     words_x = []
+     i = 0
+     while i < len(words):
+          word = words[i]
+          if word[0] == "x":
+               word_stored = words.pop(words.index(word))
+               words_x.append(word_stored)
+          else:
+               i += 1
+     words = sorted(words)
+     words_x = sorted(words_x)
+     sorted_list = words_x + words
+
+     return sorted_list
 
 
 
@@ -44,8 +59,7 @@ def front_x(words):
 
 
 def sort_last(tuples):
-    # +++your code here+++
-    return 
+    return sorted(tuples, key=lambda x: x[-1])
 
 
 # Simple provided test() function used in main() to print
